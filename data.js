2047f4603b4b3fdf018eee70b332863ca803cc1a
Srivatsan <vatsan1993@gmail.com>
2024-09-01 17:41:46 +0530

    finished project

--- data.js
+++ data.js
@@ -1,1 +1,12 @@
 // Your code here
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelector('#add').addEventListener('click', (event) => {
+    event.preventDefault();
+    let name = document.querySelector('#name').value;
+    let type = document.querySelector('#type').value;
+    let li = document.createElement('li');
+    li.textContent = name;
+    li.setAttribute('data-type', type);
+    document.querySelector('#shopping-list').appendChild(li);
+  });
+});
